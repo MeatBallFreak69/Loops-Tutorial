12b2ddeac194280329de9b24b1f2473a7f195fec
Clark Miller <glitchclick@gmail.com>
2021-03-02 08:48:31 -0500

    Add project files.

---
+++ LoopsTutorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Hello!");       
+            }
+            for(int j = 1; j <= 10;)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            for (int x = 10; x >= 1;)
+            {
+                Console.WriteLine(x);
+                x--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
